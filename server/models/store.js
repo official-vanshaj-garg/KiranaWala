9df95ca1315824886bb9b175a9bb6a53fdf9b6b5
Jaineesh Patel <jaineeshpatel73@gmail.com>
2025-04-20 19:29:15 +0530

    Fix

--- server/models/store.js
+++ server/models/store.js
@@ -1,29 +1,29 @@
 const mongoose = require("mongoose");
 
 const storeSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true,
+  name: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  category: {
+    type: String,
+    required: true,
+  },
+  products: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Product",
     },
-    description: {
-        type: String,
-        required: true,
-    },
-    category: {
-        type: String,
-        required: true,
-    },
-    products: [
-        {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: "Product",
-        },
-    ],
-    owner: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "StoreOwner",
-        required: true,
-    },
+  ],
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "StoreOwner",
+    required: true,
+  },
 });
 
 module.exports = mongoose.model("Store", storeSchema);
