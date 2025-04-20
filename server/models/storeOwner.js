67783127524637bfea329dadadd466524edcea48
Jaineesh Patel <jaineeshpatel73@gmail.com>
2025-04-20 19:01:39 +0530

    Fix lint

--- server/models/storeOwner.js
+++ server/models/storeOwner.js
@@ -67,4 +67,6 @@ storeOwnerSchema.methods.comparePassword = async function (candidatePassword) {
 };
 // --- End of added code ---
 
+const StoreOwner = mongoose.model("StoreOwner", storeOwnerSchema);
+
 module.exports = mongoose.model("StoreOwner", storeOwnerSchema);
